9dbc4894bf3b5dd7b1205b73bd7f80803710b9ee
Baixo <roon0793@gmail.com>
2026-02-08 15:06:34 -0500

    Rename Ingot to Cheeto Disabler in HTML

--- cheeto.js
+++ cheeto.js
@@ -17,12 +17,12 @@ return "";
 
 //Set base page code
 document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:image/svg+xml,<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path fill='white' d='M20.5 11H19V7c0-1.1-.9-2-2-2h-4V3.5C13 2.12 11.88 1 10.5 1S8 2.12 8 3.5V5H4c-1.1 0-1.99.9-1.99 2v3.8H3.5c1.49 0 2.7 1.21 2.7 2.7s-1.21 2.7-2.7 2.7H2V20c0 1.1.9 2 2 2h3.8v-1.5c0-1.49 1.21-2.7 2.7-2.7 1.49 0 2.7 1.21 2.7 2.7V22H17c1.1 0 2-.9 2-2v-4h1.5c1.38 0 2.5-1.12 2.5-2.5S21.88 11 20.5 11z'></path></svg>">
-<title>Ingot</title>
+<title>Cheeto Disabler</title>
 </head>
 <body ` + isPageDev() + `>
 <div class="nav">
 <div class="nav-left">
-<div class="nav-title">Ingot</div>
+<div class="nav-title">cheeto disabler</div>
 <div class="nav-right">
 <div class="nav-dev">Developer mode</div>
 <div ` + isButtonDev() + ` class="item-toggle item-toggle-dev" id="toggle" onclick="toggle(this);devMode()" onmousedown="togglePress(this, 'down')" onmouseup="togglePress(this, 'up')">
@@ -40,7 +40,7 @@ document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:im
 <div class="items-main">
 <div class="items" id="items">
 <div class="patched">Error: This may have been patched</div>
-<div class="wrongpage">You are not on the correct page.<br>To use Ingot click the button below to redirect and run the bookmarklet again.<div class="item-left-buttons" style="justify-content: center; margin: 20px;">
+<div class="wrongpage">You are not on the correct page.<br>to use cheeto disabler click the button below to redirect and run the bookmarklet again.<div class="item-left-buttons" style="justify-content: center; margin: 20px;">
 <div class="item-left-button" onclick="window.location='https://chrome.google.com/webstorex'">Redirect</div>
 </div></div>
 </div>
